4720a45a181ab38d5703cfd36b04f7322afcb718
maaztausif <maaztausif48@gmail.com>
2025-07-11 21:06:05 +0200

    Project Created

---
+++ src/main/java/com/maaz/JavaSpringBoot_Demo/JavaSpringBootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.maaz.JavaSpringBoot_Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringBootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringBootDemoApplication.class, args);
+	}
+
+}
